0268fa30be686f2c328ef054b14d16447e68bbde
Fredrik Sandberg <fredrik@advancera.se>
2016-02-20 11:38:42 +0100

    added mergin in ex2

--- src/fs222mh_assign3/sort/SortingAlgorithms.java
+++ src/fs222mh_assign3/sort/SortingAlgorithms.java
@@ -22,7 +22,7 @@ public class SortingAlgorithms {
 			int temp = out[i];
 			int j = i;
 			
-			// Swap the integers as long the left is bigger then right, and that there is a left
+			// Swap the integers as long the left is bigger then right, as long there is a left from scratch
 			while(j > 0 && out[j - 1] > temp) {
 				out[j] = out[j - 1];
 				j = j - 1;
@@ -30,6 +30,77 @@ public class SortingAlgorithms {
 			out[j] = temp;
 		}
 		return out;
+	}
+	
+	/**
+	 * Looked at http://en.wikipedia.org/wiki/Merge_sort and
+	 * 
+	 * @param in
+	 * @return
+	 */
+	public int[] mergeSort(int in[]) {
+		
+		if (in.length == 1) {
+			return in;
+		}
+		
+		int[] left = new int[in.length / 2];
+		System.arraycopy(in, 0, left, 0, in.length / 2);  // cut array and place first part in left
+		int[] right;
+		
+		// Even number
+		if (in.length % 2 == 0) {
+			right = new int[in.length / 2] ;
+			System.arraycopy(in, in.length / 2, right, 0, in.length / 2); // 
+		}
+		// Odd number
+		else {
+			right = new int[(in.length / 2) + 1];
+			System.arraycopy(in, in.length / 2, right, 0, in.length / 2 + 1);
+		}
+		
+		left = mergeSort(left); // Use recursion and split the left array
+		right = mergeSort(right); // recursion on right array
+		
+		return merge(left, right);		// use the merge function to set the arrays together again and return the result
+				
+	}
+	
+	/**
+	 * Merge to arrays
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public int[] merge(int[] left, int[] right) {
+		
+		int[] result = new int[left.length + right.length]; // Create new array to hold the two merging
+		
+		// Index to keep track of locations while merging
+		int leftIndex = 0;
+		int rightIndex = 0;
+		int resultIndex = 0;
+		
+		// Add from left array it it's lower the current index in right array, otherwise add the right index to result
+		while(leftIndex < left.length && rightIndex < right.length) {
+			if (left[leftIndex] <= right[rightIndex]) {
+				result[resultIndex += 1] = left[leftIndex += 1];
+			}
+			else {
+				result[resultIndex += 1] = right[rightIndex += 1];
+			}
+		}
+		
+		// add what is left, if any. Check left and right array. The arrays are already sorted internally so adding will be correct
+		for (int i = leftIndex; i < left.length; i += 1) {
+			result[resultIndex += 1] = left[i];
+		}
+		for ( int i = rightIndex; i < right.length; i += 1) {
+			result[resultIndex += 1] = right[i];
+		}
+		
+		return result;
+		
 	}
 
 }
